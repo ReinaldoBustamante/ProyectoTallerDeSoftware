b6797837398a31ee78e835db06215f23f17198dd
reinaldobustamante97@gmail.com <Spawn199771604621>
2019-08-03 18:41:09 -0400

    Delete

--- trabajo/src/components/horario/Form.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Form({ramosdisp}){
-    return(
-        <option value={ramosdisp.ramo}>{ramosdisp.ramo}</option> 
-
-    )
-}
-export default Form;
--- trabajo/src/components/inicio/Inicio.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-function Inicio(){
-    return(
-        <h1 className="text-center">Bienvenido Usuario</h1>
-    )
-}
-export default Inicio;
